93d3c9050ee418501d85c6ef9443f432b786c46d
Thomas ten Cate <ttencate@gmail.com>
2018-12-07 11:38:58 +0100

    Refactor to add auto-fetching

---
+++ src/bin/07.rs
@@ -0,0 +1,15 @@
+extern crate aoc;
+
+fn part1(input: &str) -> String {
+    "".to_string()
+}
+
+fn part2(input: &str) -> String {
+    "".to_string()
+}
+
+fn main() {
+    let input = aoc::get_input(2018, 7);
+    println!("Answer to part 1: {}", part1(&input));
+    println!("Answer to part 2: {}", part2(&input));
+}
