a102048335fc20e349d1729394b1b73961a94d4d
Thomas ten Cate <ttencate@gmail.com>
2018-12-07 12:09:55 +0100

    Add unit test for part 1

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -2,10 +2,17 @@ fn part1(input: &str) -> String {
     "".to_string()
 }
 
-fn part2(input: &str) -> String {
-    "".to_string()
+#[test]
+fn part1example() {
+    assert_eq!(part1("Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin."), "CABDFE");
 }
 
 fn main() {
-    aoc::main!(7, part1, part2);
+    aoc::main!(7, part1);
 }
