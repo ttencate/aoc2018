3838a365f3edd1c9e14142fb6eede7ec41bb54f5
Thomas ten Cate <ttencate@gmail.com>
2018-12-08 13:45:48 +0100

    Put scaffolding in place for future solutions

---
+++ src/bin/13.rs
@@ -0,0 +1,21 @@
+fn part1(input: &str) -> String {
+    "TODO".to_string()
+}
+
+#[test]
+fn part1example() {
+    assert_eq!(part1(""), "TODO");
+}
+
+fn part2(input: &str) -> String {
+    "TODO".to_string()
+}
+
+#[test]
+fn part2example() {
+    assert_eq!(part2(""), "TODO");
+}
+
+fn main() {
+    aoc::main(part1, part2);
+}
