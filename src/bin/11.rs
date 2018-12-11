7ecc85eeca9fab110fd3cd3f53d0aab15915326a
Thomas ten Cate <ttencate@gmail.com>
2018-12-11 14:39:54 +0100

    Solve 11b

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,28 +1,29 @@
 extern crate aoc;
 
-use aoc::geom::{Point, Rect};
+use aoc::geom::{Matrix, Point, Rect};
+use std::iter;
 
-fn power_level(cell: &Point, serial_number: i32) -> i32 {
+fn power_level(cell: Point, serial_number: i32) -> i32 {
     let rack_id = cell.x + 10;
     ((((rack_id * cell.y) + serial_number) * rack_id) / 100) % 10 - 5
 }
 
 #[test]
 fn test_power_level() {
-    assert_eq!(power_level(&Point::new(3, 5), 8), 4);
-    assert_eq!(power_level(&Point::new(122, 79), 57), -5);
-    assert_eq!(power_level(&Point::new(217, 196), 39), 0);
-    assert_eq!(power_level(&Point::new(101, 153), 71), 4);
+    assert_eq!(power_level(Point::new(3, 5), 8), 4);
+    assert_eq!(power_level(Point::new(122, 79), 57), -5);
+    assert_eq!(power_level(Point::new(217, 196), 39), 0);
+    assert_eq!(power_level(Point::new(101, 153), 71), 4);
 }
 
 fn part1(input: &str) -> Point {
     let serial_number = input.trim().parse::<i32>().unwrap();
-    Rect::new(1, 1, 300 - 2, 300 - 2)
+    Rect::from_inclusive_ranges(1 ..= 300 - 2, 1 ..= 300 - 2)
         .iter()
         .max_by_key(|cell| {
-            Rect::new(cell.x, cell.y, 3, 3)
+            Rect::from_inclusive_ranges(cell.x ..= cell.x + 2, cell.y ..= cell.y + 2)
                 .iter()
-                .map(|cell| power_level(&cell, serial_number))
+                .map(|cell| power_level(cell, serial_number))
                 .sum::<i32>()
         })
         .unwrap()
@@ -35,13 +36,42 @@ fn part1example() {
 }
 
 fn part2(input: &str) -> String {
-    // let serial_number = input.trim().parse::<i32>().unwrap();
-    "TODO".to_string()
+    let serial_number = input.trim().parse::<i32>().unwrap();
+
+    // Compute cumulative sum of all cells in the rectangle from 1,1 to x,y exclusive. O(n²).
+    let mut cum_sum = Matrix::new(&Rect::from_inclusive_ranges(1 ..= 301, 1 ..= 301), 0);
+    for cell in Rect::from_inclusive_ranges(1 ..= 300, 1 ..= 300) {
+        cum_sum[cell + Point::new(1, 1)] =
+            power_level(cell, serial_number)
+            + cum_sum[cell + Point::new(0, 1)]
+            + cum_sum[cell + Point::new(1, 0)]
+            - cum_sum[cell];
+    }
+
+    // Use cumulative sums to quickly compute the total for each possible square. O(n³). I'm not
+    // sure that a more efficient approach is possible.
+    let all_cells = Rect::from_inclusive_ranges(1 ..= 300, 1 ..= 300);
+    let (cell, size) =
+        all_cells
+        .iter()
+        .flat_map(|cell| { iter::repeat(cell).zip(1 ..= 300) })
+        .filter(|(cell, size)| {
+            all_cells.contains(*cell + Point::new(*size, *size))
+        })
+        .max_by_key(|(cell, size)| {
+            cum_sum[*cell + Point::new(*size, *size)]
+            - cum_sum[*cell + Point::new(0, *size)]
+            - cum_sum[*cell + Point::new(*size, 0)]
+            + cum_sum[*cell]
+        })
+        .unwrap();
+    format!("{},{}", cell, size)
 }
 
 #[test]
 fn part2example() {
-    assert_eq!(part2(""), "TODO");
+    assert_eq!(part2("18\n"), "90,269,16");
+    assert_eq!(part2("42\n"), "232,251,12");
 }
 
 fn main() {
--- src/geom.rs
+++ src/geom.rs
@@ -2,6 +2,7 @@ use std::cmp;
 use std::fmt::{Display, Formatter};
 use std::iter::FromIterator;
 use std::ops;
+use std::ops::RangeInclusive;
 
 // A discrete point on a 2D integer grid.
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
@@ -44,47 +45,61 @@ impl Display for Point {
     }
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Rect {
-    x: i32,
-    y: i32,
-    width: u32,
-    height: u32,
+    x_range: RangeInclusive<i32>,
+    y_range: RangeInclusive<i32>,
 }
 
 impl Rect {
-    pub fn new(x: i32, y: i32, width: u32, height: u32) -> Rect {
-        Rect { x: x, y: y, width: width, height: height }
+    pub fn from_inclusive_ranges(x_range: RangeInclusive<i32>, y_range: RangeInclusive<i32>) -> Rect {
+        Rect { x_range: x_range, y_range: y_range }
     }
 
-    pub fn x_min(&self) -> i32 { self.x }
-    pub fn x_max(&self) -> i32 { self.x + self.width as i32 - 1 }
-    pub fn y_min(&self) -> i32 { self.y }
-    pub fn y_max(&self) -> i32 { self.y + self.height as i32 - 1 }
-    pub fn width(&self) -> u32 { self.width }
-    pub fn height(&self) -> u32 { self.height }
+    pub fn x_min(&self) -> i32 { *self.x_range.start() }
+    pub fn x_max(&self) -> i32 { *self.x_range.end() }
+    pub fn y_min(&self) -> i32 { *self.y_range.start() }
+    pub fn y_max(&self) -> i32 { *self.y_range.end() }
+    pub fn width(&self) -> u32 { cmp::max(0i32, self.x_range.end() - self.x_range.start() + 1) as u32 }
+    pub fn height(&self) -> u32 { cmp::max(0i32, self.y_range.end() - self.y_range.start() + 1) as u32 }
+
+    pub fn contains(&self, point: Point) -> bool {
+        // Range::contains is nightly-only.
+        self.x_min() <= point.x && point.x <= self.x_max() && self.y_min() <= point.y && point.y <= self.y_max()
+    }
 
     pub fn iter(&self) -> RectIter {
         self.clone().into_iter()
     }
 }
 
+#[test]
+fn rect_contains_test() {
+    let rect = Rect::from_inclusive_ranges(1 ..= 3, 1 ..= 2);
+    assert!(rect.contains(Point::new(1, 1)));
+    assert!(rect.contains(Point::new(1, 1)));
+    assert!(!rect.contains(Point::new(-1, -1)));
+    assert!(!rect.contains(Point::new(0, 0)));
+    assert!(!rect.contains(Point::new(0, 1)));
+    assert!(!rect.contains(Point::new(1, 0)));
+    assert!(!rect.contains(Point::new(4, 2)));
+    assert!(!rect.contains(Point::new(3, 3)));
+    assert!(!rect.contains(Point::new(3, 4)));
+}
+
 // Allows building a Rect as a bounding box of a series of Points, e.g. using collect().
 impl<'a> FromIterator<&'a Point> for Rect {
     fn from_iter<T: IntoIterator<Item=&'a Point>>(points: T) -> Rect {
-        let (x_min, x_max, y_min, y_max) = points
+        points
             .into_iter()
             .fold(
-                (i32::max_value(), i32::min_value(), i32::max_value(), i32::min_value()),
-                |(x_min, x_max, y_min, y_max), &point| {
-                    (
-                        cmp::min(x_min, point.x), cmp::max(x_max, point.x),
-                        cmp::min(y_min, point.y), cmp::max(y_max, point.y)
+                Rect::from_inclusive_ranges(i32::max_value() ..= i32::min_value(), i32::max_value() ..= i32::min_value()),
+                |rect, &point| {
+                    Rect::from_inclusive_ranges(
+                        cmp::min(*rect.x_range.start(), point.x) ..= cmp::max(*rect.x_range.end(), point.x),
+                        cmp::min(*rect.y_range.start(), point.y) ..= cmp::max(*rect.y_range.end(), point.y)
                     )
-                });
-        let width = (x_max - x_min + 1) as u32;
-        let height = (y_max - y_min + 1) as u32;
-        Rect { x: x_min, y: y_min, width: width, height: height }
+                })
     }
 }
 
@@ -93,7 +108,7 @@ impl IntoIterator for Rect {
     type Item = Point;
     type IntoIter = RectIter;
     fn into_iter(self) -> Self::IntoIter {
-        RectIter { rect: self, current_point: Point::new(self.x_max(), self.y_min() - 1) }
+        RectIter { rect: self.clone(), current_point: Point::new(self.x_max(), self.y_min() - 1) }
     }
 }
 
@@ -111,7 +126,7 @@ impl Iterator for RectIter {
             self.current_point.x = self.rect.x_min();
             self.current_point.y += 1;
         }
-        if self.current_point.x <= self.rect.x_max() && self.current_point.y <= self.rect.y_max() {
+        if self.rect.contains(self.current_point) {
             Some(self.current_point)
         } else {
             None
@@ -121,18 +136,92 @@ impl Iterator for RectIter {
 
 #[test]
 fn rect_into_iter_test() {
-    assert_eq!(Rect { x: 0, y: 0, width: 0, height: 0 }.into_iter().collect::<Vec<Point>>(), vec![]);
-    assert_eq!(Rect { x: 0, y: 0, width: 1, height: 0 }.into_iter().collect::<Vec<Point>>(), vec![]);
-    assert_eq!(Rect { x: 0, y: 0, width: 0, height: 1 }.into_iter().collect::<Vec<Point>>(), vec![]);
-    assert_eq!(Rect { x: 0, y: 0, width: 1, height: 1 }.into_iter().collect::<Vec<Point>>(), vec![
+    assert_eq!(Rect::from_inclusive_ranges(0..=-1, 0..=-1).into_iter().collect::<Vec<Point>>(), vec![]);
+    assert_eq!(Rect::from_inclusive_ranges(0..=0, 0..=-1).into_iter().collect::<Vec<Point>>(), vec![]);
+    assert_eq!(Rect::from_inclusive_ranges(0..=-1, 0..=0).into_iter().collect::<Vec<Point>>(), vec![]);
+    assert_eq!(Rect::from_inclusive_ranges(0..=0, 0..=0).into_iter().collect::<Vec<Point>>(), vec![
         Point::new(0, 0),
     ]);
-    assert_eq!(Rect { x: 2, y: -2, width: 2, height: 1 }.into_iter().collect::<Vec<Point>>(), vec![
+    assert_eq!(Rect::from_inclusive_ranges(2..=3, -2..=-2).into_iter().collect::<Vec<Point>>(), vec![
         Point::new(2, -2),
         Point::new(3, -2),
     ]);
-    assert_eq!(Rect { x: 2, y: -2, width: 1, height: 2 }.into_iter().collect::<Vec<Point>>(), vec![
+    assert_eq!(Rect::from_inclusive_ranges(2..=2, -2..=-1).into_iter().collect::<Vec<Point>>(), vec![
         Point::new(2, -2),
         Point::new(2, -1),
     ]);
+}
+
+// A dense 2D rectangular array with customizable lower bound.
+pub struct Matrix<T> {
+    rect: Rect,
+    values: Vec<T>,
+}
+
+impl<T> Matrix<T> {
+    pub fn new(rect: &Rect, initial_value: T) -> Matrix<T>
+        where T: Clone
+    {
+        Matrix { rect: rect.clone(), values: vec![initial_value; (rect.width() * rect.height()) as usize] }
+    }
+
+    pub fn coords(&self) -> impl Iterator<Item=Point> {
+        self.rect.iter()
+    }
+
+    pub fn get(&self, point: Point) -> Option<&T> {
+        if self.rect.contains(point) {
+            Some(&self.values[self.index_of(point)])
+        } else {
+            None
+        }
+    }
+
+    pub fn get_mut(&mut self, point: Point) -> Option<&mut T> {
+        if self.rect.contains(point) {
+            let index = self.index_of(point);
+            Some(&mut self.values[index])
+        } else {
+            None
+        }
+    }
+
+    fn index_of(&self, point: Point) -> usize {
+        ((point.y - self.rect.y_min()) * self.rect.width() as i32 + point.x - self.rect.x_min()) as usize
+    }
+}
+
+impl<T> ops::Index<Point> for Matrix<T> {
+    type Output = T;
+    fn index(&self, point: Point) -> &T {
+        assert!(self.rect.contains(point));
+        &self.values[self.index_of(point)]
+    }
+}
+
+impl<T> ops::IndexMut<Point> for Matrix<T> {
+    fn index_mut(&mut self, point: Point) -> &mut T {
+        assert!(self.rect.contains(point));
+        let index = self.index_of(point);
+        &mut self.values[index]
+    }
+}
+
+#[test]
+fn test_matrix_index() {
+    let mut mat = Matrix::new(&Rect::from_inclusive_ranges(1 ..= 2, 1 ..= 2), 0);
+    mat[Point::new(1, 1)] = 1;
+    mat[Point::new(1, 2)] = 2;
+    mat[Point::new(2, 1)] = 3;
+    mat[Point::new(2, 2)] = 4;
+    assert_eq!(mat.get(Point::new(1, 1)), Some(&1));
+    assert_eq!(mat.get(Point::new(1, 2)), Some(&2));
+    assert_eq!(mat.get(Point::new(2, 1)), Some(&3));
+    assert_eq!(mat.get(Point::new(2, 2)), Some(&4));
+    assert_eq!(mat.get(Point::new(0, 0)), None);
+    assert_eq!(mat.get(Point::new(0, 1)), None);
+    assert_eq!(mat.get(Point::new(1, 0)), None);
+    assert_eq!(mat.get(Point::new(3, 2)), None);
+    assert_eq!(mat.get(Point::new(2, 3)), None);
+    assert_eq!(mat.get(Point::new(3, 3)), None);
 }
