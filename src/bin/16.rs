08000fabf17a369749baf6763c13ee1a4c172465
Thomas ten Cate <ttencate@gmail.com>
2018-12-16 14:54:09 +0100

    Solve 16b

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -1,24 +1,11 @@
 #[macro_use]
 extern crate lazy_static;
 
+use std::collections::{HashMap, HashSet};
 use std::fmt::{Display, Formatter};
 use regex::Regex;
 
 const NUM_REGISTERS: usize = 4;
-
-#[derive(Clone, Copy, Debug)]
-struct Register(usize);
-
-impl Register {
-    fn parse(s: &str) -> Option<Register> {
-        let idx = s.parse::<usize>().ok()?;
-        if idx < NUM_REGISTERS {
-            Some(Register(idx))
-        } else {
-            None
-        }
-    }
-}
 
 type Value = i32;
 
@@ -52,12 +39,28 @@ struct State {
 }
 
 impl State {
-    fn fetch(&self, reg: Register) -> Value {
-        self.registers.0[reg.0]
+    fn new() -> State {
+        State { registers: Registers([0; NUM_REGISTERS]) }
     }
 
-    fn store(&mut self, reg: Register, val: Value) {
-        self.registers.0[reg.0] = val;
+    #[allow(dead_code)]
+    fn with_registers(registers: [Value; NUM_REGISTERS]) -> State {
+        State { registers: Registers(registers) }
+    }
+
+    fn fetch(&self, reg: Value) -> Option<Value> {
+        let idx = self.register_index(reg)?;
+        Some(self.registers.0[idx])
+    }
+
+    fn store(&mut self, reg: Value, val: Value) -> Option<()> {
+        let idx = self.register_index(reg)?;
+        self.registers.0[idx] = val;
+        Some(())
+    }
+
+    fn register_index(&self, reg: Value) -> Option<usize> {
+        if reg >= 0 && (reg as usize) < NUM_REGISTERS { Some(reg as usize) } else { None }
     }
 }
 
@@ -68,107 +71,171 @@ impl Display for State {
 }
 
 #[derive(Clone, Copy, Debug)]
-enum Input {
-    Register(Register),
-    Value(Value),
-}
+struct Input(Value);
 
 impl Input {
-    fn parse_register(v: &str) -> Option<Input> {
-        Some(Input::Register(Register::parse(v)?))
+    fn reg(&self, state: &State) -> Option<Value> {
+        state.fetch(self.0)
     }
 
-    fn parse_value(v: &str) -> Option<Input> {
-        Some(Input::Value(v.parse::<Value>().ok()?))
-    }
-
-    fn eval(&self, state: &State) -> Value {
-        match self {
-            Input::Register(reg) => state.fetch(*reg),
-            Input::Value(val) => *val,
-        }
+    fn val(&self) -> Option<Value> {
+        Some(self.0)
     }
 }
 
 #[derive(Clone, Copy, Debug)]
-enum Output {
-    Register(Register),
-}
+struct Output(Value);
 
 impl Output {
-    fn parse_register(v: &str) -> Option<Output> {
-        Some(Output::Register(Register::parse(v)?))
-    }
-
-    fn store(&self, val: Value, state: &mut State) {
-        match self {
-            Output::Register(reg) => state.store(*reg, val),
-        }
+    fn store(&self, state: &mut State, val: Value) -> Option<()> {
+        state.store(self.0, val)
     }
 }
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+enum Opcode {
+    Addr,
+    Addi,
+    Mulr,
+    Muli,
+    Banr,
+    Bani,
+    Borr,
+    Bori,
+    Setr,
+    Seti,
+    Gtir,
+    Gtri,
+    Gtrr,
+    Eqir,
+    Eqri,
+    Eqrr,
+}
+
+impl ToString for Opcode {
+    fn to_string(&self) -> String {
+        format!("{:?}", self).to_lowercase()
+    }
+}
+
+const ALL_OPCODES: &[Opcode] = &[
+    Opcode::Addr,
+    Opcode::Addi,
+    Opcode::Mulr,
+    Opcode::Muli,
+    Opcode::Banr,
+    Opcode::Bani,
+    Opcode::Borr,
+    Opcode::Bori,
+    Opcode::Setr,
+    Opcode::Seti,
+    Opcode::Gtir,
+    Opcode::Gtri,
+    Opcode::Gtrr,
+    Opcode::Eqir,
+    Opcode::Eqri,
+    Opcode::Eqrr,
+];
+
 #[derive(Clone, Copy, Debug)]
-enum Instruction {
-    Add(Input, Input, Output),
-    Mul(Input, Input, Output),
-    Ban(Input, Input, Output),
-    Bor(Input, Input, Output),
-    Set(Input, Output),
-    Gt(Input, Input, Output),
-    Eq(Input, Input, Output),
+struct Instruction {
+    opcode: Opcode,
+    a: Input,
+    b: Input,
+    c: Output,
 }
 
 impl Instruction {
-    fn parse(opcode: &str, a: &str, b: &str, c: &str) -> Option<Instruction> {
-        let reg = Input::parse_register;
-        let val = Input::parse_value;
-        let out = Output::parse_register;
-        match opcode {
-            "addr" => Some(Instruction::Add(reg(a)?, reg(b)?, out(c)?)),
-            "addi" => Some(Instruction::Add(reg(a)?, val(b)?, out(c)?)),
-            "mulr" => Some(Instruction::Mul(reg(a)?, reg(b)?, out(c)?)),
-            "muli" => Some(Instruction::Mul(reg(a)?, val(b)?, out(c)?)),
-            "banr" => Some(Instruction::Ban(reg(a)?, reg(b)?, out(c)?)),
-            "bani" => Some(Instruction::Ban(reg(a)?, val(b)?, out(c)?)),
-            "borr" => Some(Instruction::Bor(reg(a)?, reg(b)?, out(c)?)),
-            "bori" => Some(Instruction::Bor(reg(a)?, val(b)?, out(c)?)),
-            "setr" => Some(Instruction::Set(reg(a)?, out(c)?)),
-            "seti" => Some(Instruction::Set(val(a)?, out(c)?)),
-            "gtir" => Some(Instruction::Gt(val(a)?, reg(b)?, out(c)?)),
-            "gtri" => Some(Instruction::Gt(reg(a)?, val(b)?, out(c)?)),
-            "gtrr" => Some(Instruction::Gt(reg(a)?, reg(b)?, out(c)?)),
-            "eqir" => Some(Instruction::Eq(val(a)?, reg(b)?, out(c)?)),
-            "eqri" => Some(Instruction::Eq(reg(a)?, val(b)?, out(c)?)),
-            "eqrr" => Some(Instruction::Eq(reg(a)?, reg(b)?, out(c)?)),
-            _ => None
-        }
-    }
-
-    fn parse_multiple(line: &str) -> Vec<Instruction> {
-        if let [_opcode, input_a, input_b, output] = line.split_whitespace()
-            .collect::<Vec<&str>>()
+    fn parse_line(line: &str) -> Option<(usize, Value, Value, Value)> {
+        if let [Some(opcode_idx), Some(a), Some(b), Some(c)] = line.split_whitespace()
+            .map(|v| v.parse::<Value>().ok())
+            .collect::<Vec<Option<Value>>>()
             .as_slice()
         {
-            ["addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtir", "gtri", "gtrr", "eqir", "eqri", "eqrr"]
-                .iter()
-                .filter_map(|opcode| Self::parse(opcode, input_a, input_b, output))
-                .collect()
+            Some((*opcode_idx as usize, *a, *b, *c))
         } else {
-            vec![]
+            None
         }
     }
 
-    fn execute(&self, state: &mut State) {
-        match self {
-            Instruction::Add(a, b, c) => c.store(a.eval(state) + b.eval(state), state),
-            Instruction::Mul(a, b, c) => c.store(a.eval(state) * b.eval(state), state),
-            Instruction::Ban(a, b, c) => c.store(a.eval(state) & b.eval(state), state),
-            Instruction::Bor(a, b, c) => c.store(a.eval(state) | b.eval(state), state),
-            Instruction::Set(a, c) => c.store(a.eval(state), state),
-            Instruction::Gt(a, b, c) => c.store(if a.eval(state) > b.eval(state) { 1 } else { 0 }, state),
-            Instruction::Eq(a, b, c) => c.store(if a.eval(state) == b.eval(state) { 1 } else { 0 }, state),
+    #[allow(dead_code)]
+    fn parse(line: &str) -> Option<Instruction> {
+        if let [opcode_str, a, b, c] = line.split_whitespace().collect::<Vec<&str>>().as_slice() {
+            let opcode = ALL_OPCODES.iter().find(|opcode| opcode.to_string() == *opcode_str)?;
+            Some(Instruction {
+                opcode: *opcode,
+                a: Input(a.parse::<Value>().ok()?),
+                b: Input(b.parse::<Value>().ok()?),
+                c: Output(c.parse::<Value>().ok()?),
+            })
+        } else {
+            None
         }
     }
+
+    fn parse_with_opcode_map(opcode_map: &HashMap<usize, Opcode>, line: &str) -> Option<Instruction> {
+        Self::parse_line(line)
+            .and_then(|(opcode_idx, a, b, c)| {
+                Some(Instruction {
+                    opcode: *(opcode_map.get(&opcode_idx)?),
+                    a: Input(a),
+                    b: Input(b),
+                    c: Output(c),
+                })
+            })
+    }
+
+    fn parse_multiple(line: &str) -> Option<(usize, Vec<Instruction>)> {
+        Self::parse_line(line)
+            .map(|(opcode_idx, a, b, c)| {
+                (
+                    opcode_idx,
+                    ALL_OPCODES.iter()
+                        .map(|opcode| {
+                            Instruction {
+                                opcode: *opcode,
+                                a: Input(a),
+                                b: Input(b),
+                                c: Output(c),
+                            }
+                        })
+                        .collect()
+                )
+            })
+    }
+
+    fn execute(&self, state: &mut State) -> Option<()> {
+        let a = self.a;
+        let b = self.b;
+        let c = self.c;
+        match self.opcode {
+            Opcode::Addr => c.store(state, a.reg(state)? + b.reg(state)?)?,
+            Opcode::Addi => c.store(state, a.reg(state)? + b.val()?)?,
+            Opcode::Mulr => c.store(state, a.reg(state)? * b.reg(state)?)?,
+            Opcode::Muli => c.store(state, a.reg(state)? * b.val()?)?,
+            Opcode::Banr => c.store(state, a.reg(state)? & b.reg(state)?)?,
+            Opcode::Bani => c.store(state, a.reg(state)? & b.val()?)?,
+            Opcode::Borr => c.store(state, a.reg(state)? | b.reg(state)?)?,
+            Opcode::Bori => c.store(state, a.reg(state)? | b.val()?)?,
+            Opcode::Setr => c.store(state, a.reg(state)?)?,
+            Opcode::Seti => c.store(state, a.val()?)?,
+            Opcode::Gtir => c.store(state, if a.val()? > b.reg(state)? { 1 } else { 0 })?,
+            Opcode::Gtri => c.store(state, if a.reg(state)? > b.val()? { 1 } else { 0 })?,
+            Opcode::Gtrr => c.store(state, if a.reg(state)? > b.reg(state)? { 1 } else { 0 })?,
+            Opcode::Eqir => c.store(state, if a.val()? == b.reg(state)? { 1 } else { 0 })?,
+            Opcode::Eqri => c.store(state, if a.reg(state)? == b.val()? { 1 } else { 0 })?,
+            Opcode::Eqrr => c.store(state, if a.reg(state)? == b.reg(state)? { 1 } else { 0 })?,
+        }
+        Some(())
+    }
+}
+
+#[allow(dead_code)]
+#[inline]
+fn test_instruction(regs_before: [Value; NUM_REGISTERS], instr_str: &str, regs_after: [Value; NUM_REGISTERS]) {
+    let mut state = State::with_registers(regs_before);
+    Instruction::parse(instr_str).unwrap().execute(&mut state);
+    assert_eq!(state.registers.0, regs_after);
 }
 
 fn part1(input: &str) -> u32 {
@@ -179,7 +246,7 @@ fn part1(input: &str) -> u32 {
             continue;
         }
         if let Some(regs_before) = Registers::parse(line) {
-            let instructions = Instruction::parse_multiple(lines.next().unwrap());
+            let (_opcode_idx, instructions) = Instruction::parse_multiple(lines.next().unwrap()).unwrap();
             let regs_after = Registers::parse(lines.next().unwrap()).unwrap();
             if instructions
                 .iter()
@@ -206,13 +273,58 @@ fn part1example() {
 After:  [3, 2, 2, 1]"), 1);
 }
 
-fn part2(_input: &str) -> String {
-    "TODO".to_string()
-}
+fn part2(input: &str) -> Value {
+    let mut lines = input.lines();
+    let mut blank_lines = 0;
 
-#[test]
-fn part2example() {
-    assert_eq!(part2(""), "TODO");
+    let mut candidates: HashMap<usize, HashSet<Opcode>> =
+        (0..ALL_OPCODES.len())
+        .map(|idx| (idx, ALL_OPCODES.iter().map(|opcode| *opcode).collect()))
+        .collect();
+
+    while blank_lines < 3 {
+        let line = lines.next().unwrap();
+        if line.len() == 0 {
+            blank_lines += 1;
+            continue;
+        }
+        blank_lines = 0;
+
+        let regs_before = Registers::parse(line).unwrap();
+        let (opcode_idx, instructions) = Instruction::parse_multiple(lines.next().unwrap()).unwrap();
+        let regs_after = Registers::parse(lines.next().unwrap()).unwrap();
+
+        for instr in instructions {
+            let mut state = State { registers: regs_before.clone() };
+            if instr.execute(&mut state).is_none() || state.registers != regs_after {
+                candidates.get_mut(&opcode_idx).unwrap().remove(&instr.opcode);
+            }
+        }
+    }
+
+    for (k, v) in candidates.iter() { println!("{:2} -> {:?}", k, v); }
+
+    let mut opcode_map = HashMap::<usize, Opcode>::new();
+    while !candidates.is_empty() {
+        let (&idx, opcodes) = candidates
+            .iter()
+            .find(|(_idx, opcodes)| opcodes.len() == 1)
+            .unwrap();
+        let identified_opcode = *opcodes.iter().next().unwrap();
+        opcode_map.insert(idx, identified_opcode);
+        for opcodes in candidates.values_mut() {
+            opcodes.remove(&identified_opcode);
+        }
+        candidates.remove(&idx);
+    }
+
+    for (k, v) in opcode_map.iter() { println!("{:2} -> {:?}", k, v); }
+
+    let mut state = State::new();
+    for line in lines {
+        Instruction::parse_with_opcode_map(&opcode_map, line).unwrap().execute(&mut state);
+    }
+    state.fetch(0).unwrap()
 }
 
 fn main() {
