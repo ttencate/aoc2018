457d019d98ba5a56393b2fa6cb0faa4f8ede5e8b
Thomas ten Cate <ttencate@gmail.com>
2018-12-21 12:57:29 +0100

    Decompile do-while statements

--- src/vm/decompiler.rs
+++ src/vm/decompiler.rs
@@ -116,48 +116,63 @@ enum Statement {
     Assignment(Variable, Expression),
     OpAssignment(Variable, Operator, Operand),
     If(Expression, Box<Block>),
+    DoWhile(Box<Block>, Expression),
     Goto(Label),
     Exit(),
+    NoOp(),
 }
 
 #[derive(Clone)]
 struct LabelledStatement {
     idx: usize,
     label: Option<Label>,
-    depth: usize,
     stat: Statement,
-}
-
-impl Display for LabelledStatement {
-    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-        let label = if let Some(label) = &self.label {
-            format!("{:>2}:", label.to_string())
-        } else {
-            "".to_string()
-        };
-        write!(f, "{:5}", label)?;
-        let indent = "    ".repeat(self.depth);
-        write!(f, "{}", indent)?;
-        match &self.stat {
-            Statement::Assignment(var, expr) => write!(f, "{} = {};", var, expr)?,
-            Statement::OpAssignment(var, op, oper) => write!(f, "{} {}= {};", var, op, oper)?,
-            Statement::If(cond, block) => write!(f, "if {} {{\n{}     {}}}", cond, block, indent)?,
-            Statement::Goto(label) => write!(f, "goto {};", label)?,
-            Statement::Exit() => write!(f, "exit();")?,
-        }
-        Ok(())
-    }
 }
 
 #[derive(Clone)]
 pub struct Block {
+    depth: usize,
     statements: Vec<LabelledStatement>,
+}
+
+impl Block {
+    fn find_label_index(&self, label: Label) -> Option<usize> {
+        (0..self.statements.len()).find(|&i| self.statements[i].label == Some(label))
+    }
+
+    fn increase_depth(&mut self) {
+        self.depth += 1;
+        for statement in &mut self.statements {
+            match &mut statement.stat {
+                Statement::If(_, body) => body.increase_depth(),
+                Statement::DoWhile(body, _) => body.increase_depth(),
+                _ => {}
+            }
+        }
+    }
 }
 
 impl Display for Block {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-        for statement in &self.statements {
-            write!(f, "{}\n", statement)?;
+        let indent = "    ".repeat(self.depth);
+        for labelled_statement in &self.statements {
+            let label = if let Some(label) = &labelled_statement.label {
+                format!("{:>2}:", label.to_string())
+            } else {
+                "".to_string()
+            };
+            write!(f, "{:5}", label)?;
+            write!(f, "{}", indent)?;
+            match &labelled_statement.stat {
+                Statement::Assignment(var, expr) => write!(f, "{} = {};", var, expr)?,
+                Statement::OpAssignment(var, op, oper) => write!(f, "{} {}= {};", var, op, oper)?,
+                Statement::If(cond, block) => write!(f, "if {} {{\n{}     {}}}", cond, block, indent)?,
+                Statement::DoWhile(block, cond) => write!(f, "do {{\n{}     {}}} while {};", block, indent, cond)?,
+                Statement::Goto(label) => write!(f, "goto {};", label)?,
+                Statement::Exit() => write!(f, "exit();")?,
+                Statement::NoOp() => {}
+            }
+            write!(f, "\n")?;
         }
         Ok(())
     }
@@ -190,11 +205,12 @@ impl<'a> Decompiler<'a> {
             .enumerate()
             .map(|(idx, instr)| self.instruction_to_labelled_statement(idx, instr))
             .collect();
-        let mut program = Block { statements: statements };
+        let mut program = Block { depth: 0, statements: statements };
         self.add_op_assignments(&mut program);
         self.add_gotos(&mut program);
-        self.strip_unused_labels(&mut program);
+        self.strip_unused_labels(&mut program); // Labels may stop "if"s from being detected.
         self.add_ifs(&mut program);
+        self.add_do_whiles(&mut program);
         self.strip_unused_labels(&mut program);
         program
     }
@@ -202,7 +218,7 @@ impl<'a> Decompiler<'a> {
     fn instruction_to_labelled_statement(&self, idx: usize, instr: &Instruction) -> LabelledStatement {
         let val = |val| Operand::Value(val);
         let var = |val| Operand::Variable(self.var(val));
-        let ass = |lval, expr| LabelledStatement { idx: idx, label: Some(Label(idx)), depth: 0, stat: Statement::Assignment(lval, expr) };
+        let ass = |lval, expr| LabelledStatement { idx: idx, label: Some(Label(idx)), stat: Statement::Assignment(lval, expr) };
         let bin_op = |lhs, op, rhs| Expression::BinaryOp(lhs, op, rhs);
         let a = instr.a().raw();
         let b = instr.b().raw();
@@ -268,6 +284,9 @@ impl<'a> Decompiler<'a> {
                 Statement::If(_, body) => {
                     self.find_used_labels(body, used_labels);
                 }
+                Statement::DoWhile(body, _) => {
+                    self.find_used_labels(body, used_labels);
+                }
                 _ => {}
             }
         }
@@ -276,49 +295,107 @@ impl<'a> Decompiler<'a> {
     fn strip_unused_labels(&self, program: &mut Block) {
         let mut used_labels = HashSet::new();
         self.find_used_labels(program, &mut used_labels);
-        for statement in &mut program.statements {
+        self.strip_labels_outside_set(program, &used_labels);
+    }
+
+    fn strip_labels_outside_set(&self, block: &mut Block, used_labels: &HashSet<Label>) {
+        for statement in &mut block.statements {
             if let Some(label) = &statement.label {
                 if !used_labels.contains(&label) {
                     statement.label = None;
                 }
             }
+            match &mut statement.stat {
+                Statement::If(_, body) => {
+                    self.strip_labels_outside_set(body, used_labels);
+                }
+                Statement::DoWhile(body, _) => {
+                    self.strip_labels_outside_set(body, used_labels);
+                }
+                _ => {}
+            }
         }
     }
 
-    fn add_ifs(&self, program: &mut Block) {
+    fn add_ifs(&self, block: &mut Block) {
         let mut i = 0;
-        while i < program.statements.len() - 2 {
-            let fst = &program.statements[i];
-            let snd = &program.statements[i + 1];
-            let thd = &program.statements[i + 2];
+        while i < block.statements.len() - 2 {
+            let fst = &block.statements[i];
+            let snd = &block.statements[i + 1];
+            let thd = &block.statements[i + 2];
             if let Statement::Assignment(Variable::Named(fst_var), Expression::BinaryOp(lhs, op, rhs)) = &fst.stat {
                 if op.is_conditional() {
                     if let Statement::OpAssignment(Variable::InstructionPointer(), Operator::Add, Operand::Variable(Variable::Named(snd_var))) = &snd.stat {
                         if fst_var == snd_var {
                             if let Statement::Goto(_) = &thd.stat {
                                 if snd.label.is_none() {
-                                    program.statements[i] = LabelledStatement {
+                                    block.statements[i] = LabelledStatement {
                                         idx: fst.idx,
                                         label: fst.label,
-                                        depth: fst.depth,
                                         stat: Statement::If(Expression::BinaryOp(lhs.clone(), op.invert(), rhs.clone()), Box::new(Block {
+                                            depth: block.depth + 1,
                                             statements: vec![
                                                 LabelledStatement {
                                                     idx: thd.idx,
                                                     label: thd.label,
-                                                    depth: thd.depth + 1,
                                                     stat: thd.stat.clone(),
                                                 },
                                             ],
                                         })),
                                     };
-                                    program.statements.remove(i + 2);
-                                    program.statements.remove(i + 1);
+                                    block.statements.remove(i + 2);
+                                    block.statements.remove(i + 1);
                                 }
                             }
                         }
                     }
                 }
+            }
+            i += 1;
+        }
+    }
+
+    fn add_do_whiles(&self, block: &mut Block) {
+        let mut i = 0;
+        while i < block.statements.len() {
+            match &block.statements[i].stat {
+                Statement::If(if_cond, if_body) => {
+                    if if_body.statements.len() == 1 {
+                        if let Statement::Goto(label) = if_body.statements[0].stat {
+                            if let Some(j) = block.find_label_index(label) {
+                                if j <= i {
+                                    let mut do_while_body = Box::new(Block {
+                                        depth: block.depth,
+                                        statements: (j..i).map(|k| { block.statements[k].clone() }).collect(),
+                                    });
+                                    do_while_body.increase_depth();
+                                    if block.statements[i].label.is_some() {
+                                        do_while_body.statements.push(LabelledStatement {
+                                            idx: block.statements[i].idx,
+                                            label: block.statements[i].label,
+                                            stat: Statement::NoOp(),
+                                        });
+                                    }
+                                    self.add_do_whiles(do_while_body.as_mut());
+                                    let do_while_statement = LabelledStatement {
+                                        idx: block.statements[j].idx,
+                                        label: block.statements[j].label,
+                                        stat: Statement::DoWhile(do_while_body, if_cond.clone()),
+                                    };
+                                    block.statements.splice(j..=i, std::iter::once(do_while_statement));
+                                    i = j;
+                                }
+                            }
+                        }
+                    }
+                }
+                _ => {}
+            }
+            match &mut block.statements[i].stat {
+                Statement::DoWhile(body, _) => {
+                    self.add_do_whiles(body.as_mut());
+                }
+                _ => {}
             }
             i += 1;
         }
@@ -389,12 +466,30 @@ seti 123 0 1
 bani 1 456 1
 eqri 1 72 1
 addr 1 3 3
+seti 4 0 3
+seti 0 0 1").decompile().to_string(), "     b = 123;
+     b &= 456;
+     if b != 72 {
+         goto 5;
+     }
+ 5:  b = 0;
+");
+}
+
+#[test]
+fn test_do_while() {
+    assert_eq!(Program::parse("#ip 3
+seti 123 0 1
+bani 1 456 1
+eqri 1 72 1
+addr 1 3 3
 seti 0 0 3
 seti 0 0 1").decompile().to_string(), "     b = 123;
- 1:  b &= 456;
-     if b != 72 {
-         goto 1;
-     }
+     do {
+         b &= 456;
+     } while b != 72;
      b = 0;
 ");
+
+
 }
